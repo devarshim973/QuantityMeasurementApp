abf5a6c7df28a1d7814c66d97dd448e2789ce7a5
devarshim973 <devarshim973@gmail.com>
2026-02-23 13:32:16 +0530

    [devarshi]LengthUnit.java

--- src/main/java/com/quantity/domain/length/LengthUnit.java
+++ src/main/java/com/quantity/domain/length/LengthUnit.java
@@ -1,9 +1,12 @@
+
 package com.quantity.domain.length;
 
 public enum LengthUnit {
 
-    FEET(1.0),
-    INCH(1.0 / 12.0);
+    FEET(1.0),                 // Base Unit
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(1.0/30.48);
 
     private final double conversionFactorToFeet;
 
@@ -11,7 +14,25 @@ public enum LengthUnit {
         this.conversionFactorToFeet = conversionFactorToFeet;
     }
 
-    public double toBaseUnit(double value) {
+    public double getConversionFactor() {
+        return conversionFactorToFeet;
+    }
+
+    //Convert value in THIS unit → base unit (feet)
+    public double convertToBaseUnit(double value) {
+
+        if(!Double.isFinite(value))
+            throw new IllegalArgumentException("Invalid numeric value");
+
         return value * conversionFactorToFeet;
+    }
+
+    //Convert value from base unit (feet) → THIS unit
+    public double convertFromBaseUnit(double baseValue) {
+
+        if(!Double.isFinite(baseValue))
+            throw new IllegalArgumentException("Invalid numeric value");
+
+        return baseValue / conversionFactorToFeet;
     }
 }
