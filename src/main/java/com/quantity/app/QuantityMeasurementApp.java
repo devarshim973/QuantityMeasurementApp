4e4d69e1d5e1ed2fa254967af9539c9dbd67a684
devarshim973 <devarshim973@gmail.com>
2026-02-23 12:15:26 +0530

    [devarshi]QuantityMeasurementApp.java

--- src/main/java/com/quantity/app/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/app/QuantityMeasurementApp.java
@@ -1,29 +1,30 @@
 package com.quantity.app;
+
 import com.quantity.domain.length.LengthUnit;
 import com.quantity.domain.length.Quantity;
 
 public class QuantityMeasurementApp {
 
-    public static boolean demonstrateLengthEquality(Quantity q1, Quantity q2) {
-        return q1.equals(q2);
-    }
-
-    public static void demonstrate(String label, Quantity q1, Quantity q2) {
-        System.out.println(label + " : " + demonstrateLengthEquality(q1, q2));
+    private static void demonstrate(String label, Quantity q1, Quantity q2) {
+        System.out.println(label + " : " + q1.equals(q2));
     }
 
     public static void main(String[] args) {
 
-        demonstrate("Feet == Feet",
-                new Quantity(1.0, LengthUnit.FEET),
-                new Quantity(1.0, LengthUnit.FEET));
+        demonstrate("Yard == Feet",
+                new Quantity(1.0, LengthUnit.YARD),
+                new Quantity(3.0, LengthUnit.FEET));
 
-        demonstrate("Inches == Inches",
-                new Quantity(1.0, LengthUnit.INCH),
-                new Quantity(1.0, LengthUnit.INCH));
+        demonstrate("Yard == Inches",
+                new Quantity(1.0, LengthUnit.YARD),
+                new Quantity(36.0, LengthUnit.INCH));
 
-        demonstrate("Feet == Inches",
-                new Quantity(1.0, LengthUnit.FEET),
-                new Quantity(12.0, LengthUnit.INCH));
+        demonstrate("CM == Inches",
+                new Quantity(1.0, LengthUnit.CENTIMETER),
+                new Quantity(0.393701, LengthUnit.INCH));
+
+        demonstrate("2 Yard == 6 Feet",
+                new Quantity(2.0, LengthUnit.YARD),
+                new Quantity(6.0, LengthUnit.FEET));
     }
 }
