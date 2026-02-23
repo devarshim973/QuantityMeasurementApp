6baad6eb19dcce6dbc161570a7106f97c610a5dc
devarshim973 <devarshim973@gmail.com>
2026-02-23 12:16:06 +0530

    [devarshi]LengthUnit.java

--- src/main/java/com/quantity/domain/length/LengthUnit.java
+++ src/main/java/com/quantity/domain/length/LengthUnit.java
@@ -3,7 +3,9 @@ package com.quantity.domain.length;
 public enum LengthUnit {
 
     FEET(1.0),
-    INCH(1.0 / 12.0);
+    INCH(1.0 / 12.0),
+    YARD(3.0),
+    CENTIMETER(0.0328084167);
 
     private final double conversionFactorToFeet;
 
