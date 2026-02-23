6b1896beff56179110867c1c6316730d1e2f04a5
devarshim973 <devarshim973@gmail.com>
2026-02-23 13:10:27 +0530

    [devarshi]LengthUnit.java

--- src/main/java/com/quantity/domain/length/LengthUnit.java
+++ src/main/java/com/quantity/domain/length/LengthUnit.java
@@ -2,16 +2,22 @@ package com.quantity.domain.length;
 
 public enum LengthUnit {
 
-    FEET(1.0),
-    INCH(1.0 / 12.0);
+    FEET(1.0),                   // Base Unit
+    INCHES(1.0 / 12.0),          // 1 inch = 1/12 feet
+    YARDS(3.0),                  // 1 yard = 3 feet
+    CENTIMETERS(0.0328084);      // 1 cm = 0.0328084 feet
 
-    private final double conversionFactorToFeet;
+    private final double toFeetFactor;
 
-    LengthUnit(double conversionFactorToFeet) {
-        this.conversionFactorToFeet = conversionFactorToFeet;
+    LengthUnit(double toFeetFactor) {
+        this.toFeetFactor = toFeetFactor;
     }
 
-    public double toBaseUnit(double value) {
-        return value * conversionFactorToFeet;
+    public double toFeet(double value) {
+        return value * toFeetFactor;
+    }
+
+    public double fromFeet(double feetValue) {
+        return feetValue / toFeetFactor;
     }
 }
