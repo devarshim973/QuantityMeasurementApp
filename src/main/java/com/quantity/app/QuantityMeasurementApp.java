12bb3dbc9cd05b9b90e88d0045e251014e9858c1
devarshim973 <devarshim973@gmail.com>
2026-02-23 15:12:27 +0530

    [devarshi]QuantityMeasurementApp.java

--- src/main/java/com/quantity/app/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/app/QuantityMeasurementApp.java
@@ -1,40 +1,42 @@
-
 package com.quantity.app;
 
+import com.quantity.domain.Quantity;
 import com.quantity.domain.length.LengthUnit;
-import com.quantity.domain.length.Quantity;
+import com.quantity.domain.weight.WeightUnit;
 
 public class QuantityMeasurementApp {
 
+    public static <U extends com.quantity.domain.IMeasurable>
+    void demonstrateEquality(Quantity<U> q1, Quantity<U> q2) {
+        System.out.println(q1.equals(q2));
+    }
+
+    public static <U extends com.quantity.domain.IMeasurable>
+    void demonstrateConversion(Quantity<U> q, U targetUnit) {
+        System.out.println(q.convertTo(targetUnit));
+    }
+
+    public static <U extends com.quantity.domain.IMeasurable>
+    void demonstrateAddition(Quantity<U> q1, Quantity<U> q2, U targetUnit) {
+        System.out.println(q1.add(q2, targetUnit));
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(
-                new Quantity(1.0, LengthUnit.FEET)
-                        .convertTo(LengthUnit.INCHES)
-        );
+        // Length
+        Quantity<LengthUnit> l1 = new Quantity<>(1.0, LengthUnit.FEET);
+        Quantity<LengthUnit> l2 = new Quantity<>(12.0, LengthUnit.INCHES);
 
-        System.out.println(
-                Quantity.add(
-                        new Quantity(1.0, LengthUnit.FEET),
-                        new Quantity(12.0, LengthUnit.INCHES),
-                        LengthUnit.FEET)
-        );
+        demonstrateEquality(l1, l2);
+        demonstrateConversion(l1, LengthUnit.INCHES);
+        demonstrateAddition(l1, l2, LengthUnit.FEET);
 
-        System.out.println(
-                new Quantity(36.0, LengthUnit.INCHES)
-                        .equals(new Quantity(1.0, LengthUnit.YARDS))
-        );
+        // Weight
+        Quantity<WeightUnit> w1 = new Quantity<>(1.0, WeightUnit.KILOGRAM);
+        Quantity<WeightUnit> w2 = new Quantity<>(1000.0, WeightUnit.GRAM);
 
-        System.out.println(
-                Quantity.add(
-                        new Quantity(1.0, LengthUnit.YARDS),
-                        new Quantity(3.0, LengthUnit.FEET),
-                        LengthUnit.YARDS)
-        );
-
-        System.out.println(
-                new Quantity(2.54, LengthUnit.CENTIMETERS)
-                        .convertTo(LengthUnit.INCHES)
-        );
+        demonstrateEquality(w1, w2);
+        demonstrateConversion(w1, WeightUnit.GRAM);
+        demonstrateAddition(w1, w2, WeightUnit.KILOGRAM);
     }
 }
