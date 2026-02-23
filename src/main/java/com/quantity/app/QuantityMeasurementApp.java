8e6af959b05974af60c7c0d764eca7332cccc8ae
devarshim973 <devarshim973@gmail.com>
2026-02-23 13:31:02 +0530

    [devarshi]QuantityMeasurementApp.java

--- src/main/java/com/quantity/app/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/app/QuantityMeasurementApp.java
@@ -1,29 +1,40 @@
+
 package com.quantity.app;
+
 import com.quantity.domain.length.LengthUnit;
 import com.quantity.domain.length.Quantity;
 
 public class QuantityMeasurementApp {
 
-    public static boolean demonstrateLengthEquality(Quantity q1, Quantity q2) {
-        return q1.equals(q2);
-    }
-
-    public static void demonstrate(String label, Quantity q1, Quantity q2) {
-        System.out.println(label + " : " + demonstrateLengthEquality(q1, q2));
-    }
-
     public static void main(String[] args) {
 
-        demonstrate("Feet == Feet",
-                new Quantity(1.0, LengthUnit.FEET),
-                new Quantity(1.0, LengthUnit.FEET));
+        System.out.println(
+                new Quantity(1.0, LengthUnit.FEET)
+                        .convertTo(LengthUnit.INCHES)
+        );
 
-        demonstrate("Inches == Inches",
-                new Quantity(1.0, LengthUnit.INCH),
-                new Quantity(1.0, LengthUnit.INCH));
+        System.out.println(
+                Quantity.add(
+                        new Quantity(1.0, LengthUnit.FEET),
+                        new Quantity(12.0, LengthUnit.INCHES),
+                        LengthUnit.FEET)
+        );
 
-        demonstrate("Feet == Inches",
-                new Quantity(1.0, LengthUnit.FEET),
-                new Quantity(12.0, LengthUnit.INCH));
+        System.out.println(
+                new Quantity(36.0, LengthUnit.INCHES)
+                        .equals(new Quantity(1.0, LengthUnit.YARDS))
+        );
+
+        System.out.println(
+                Quantity.add(
+                        new Quantity(1.0, LengthUnit.YARDS),
+                        new Quantity(3.0, LengthUnit.FEET),
+                        LengthUnit.YARDS)
+        );
+
+        System.out.println(
+                new Quantity(2.54, LengthUnit.CENTIMETERS)
+                        .convertTo(LengthUnit.INCHES)
+        );
     }
 }
