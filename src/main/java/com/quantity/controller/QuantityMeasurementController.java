c38bbc3aefcd6f804846e691535220a4fc58de31
devarshim973 <devarshim973@gmail.com>
2026-04-02 12:28:44 +0530

    [devarshi] add file

--- src/main/java/com/quantity/controller/QuantityMeasurementController.java
+++ src/main/java/com/quantity/controller/QuantityMeasurementController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/quantities")
 @Tag(name = "Quantity Measurements", description = "REST API for quantity measurement operations")
